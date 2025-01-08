55e355491db52e434f7475211d2fc86c56484457
Eduarda Calado <dudacal1600@hotmail.com>
2025-01-08 08:12:46 -0300

    feat: create a simple hello world

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div>
+      <h1>
+        Hello, world! 👋
+      </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
