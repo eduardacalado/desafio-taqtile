cc7e9c1589f5ac1af05d4dadd537b64c499dcc4c
Eduarda Calado <dudacal1600@hotmail.com>
2025-01-14 13:31:14 -0300

    docs: add apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 );
