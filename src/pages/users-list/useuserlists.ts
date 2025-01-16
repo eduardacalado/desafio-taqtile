69829dab147ea92a971e913366e28f8e9d867be3
Eduarda Calado <dudacal1600@hotmail.com>
2025-01-16 20:47:09 -0300

    refactor: change hook name

---
+++ src/pages/users-list/useuserlists.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services';
+
+export type UsersProps = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+export function useUserTable() {
+  const [users, setUsers] = useState<UsersProps[]>([]);
+
+  useEffect(() => {
+    api.get('users').then((reponse) => {
+      setUsers(reponse.data.users);
+    });
+  }, []);
+
+  return { users };
+}
