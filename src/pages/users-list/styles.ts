ca366f554bc137ad9a4d51dc30fe64bb0d5817f3
Eduarda Calado <dudacal1600@hotmail.com>
2025-01-16 12:37:37 -0300

    refactor: implement theme in styles

--- src/pages/users-list/styles.ts
+++ src/pages/users-list/styles.ts
@@ -8,7 +8,7 @@ export const ScreenContainer = styled.div`
   height: 100vh;
   width: 100%;
   background-color: ${themeStyled.colors.lightGray};
-  padding: 0 20px;
+  padding: 0 ${themeStyled.size.m6};
 
   align-items: center;
   justify-content: center;
@@ -24,35 +24,35 @@ export const Container = styled.div`
 
   table {
     width: 100%;
-    border-spacing: 0 1rem;
+    border-spacing: 0 ${themeStyled.size.m4};
 
     th {
       color: gray;
       font-weight: 400;
-      padding: 1rem 2rem;
+      padding: ${themeStyled.size.m4} ${themeStyled.size.m8};
       text-align: left;
-      line-height: 1.5rem;
+      line-height: ${themeStyled.size.m7};
     }
 
     tr {
       background: ${themeStyled.colors.white};
-      border-radius: 10px;
+      border-radius: ${themeStyled.size.m1};
       box-shadow: 0 0 0 2px ${themeStyled.colors.primary[700]};
     }
 
     td {
-      padding: 18px 16px;
+      padding: ${themeStyled.size.m5} ${themeStyled.size.m4};
       color: ${themeStyled.colors.dark[700]};
 
       &:first-child {
         color: ${themeStyled.colors.primary[700]};
-        border-top-left-radius: 10px;
-        border-bottom-left-radius: 10px;
+        border-top-left-radius: ${themeStyled.size.m1};
+        border-bottom-left-radius: ${themeStyled.size.m1};
       }
 
       &:last-child {
-        border-top-right-radius: 10px;
-        border-bottom-right-radius: 10px;
+        border-top-right-radius: ${themeStyled.size.m1};
+        border-bottom-right-radius: ${themeStyled.size.m1};
       }
     }
   }
